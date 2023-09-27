a22ce90cbd9d2d55c059fc7cd96feb2de4aefde2
ErykTavares <erictavares35@gmail.com>
2023-09-27 18:06:31 -0300

    fix (components/circle): adjust size

--- src/components/circle/index.jsx
+++ src/components/circle/index.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import {
-	widthPercentageToDP,
-	heightPercentageToDP,
-} from "react-native-responsive-screen";
 
 const Circle = () => {
-	const width = widthPercentageToDP("100%");
-	const height = heightPercentageToDP("100%");
-	const size = Math.min(width, height) * 0.066;
-
 	const styles = StyleSheet.create({
 		circle: {
-			width: size,
-			height: size,
-			borderRadius: size / 2,
+			width: 70,
+			height: 70,
+			borderRadius: 70,
 			backgroundColor: "red",
 		},
 	});
